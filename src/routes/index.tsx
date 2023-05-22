3641af1df7036f07ed1aa2704df3a9126a605bcb
JoaoPrestupa <joao_prestupa11@hotmail.com>
2023-05-22 13:52:35 -0300

    feat: ponto de criacao dos themes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export { };
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/pagina-inicial" element={<Button>Teste</Button>}/>
+
+            {/* Caso na renderização do AppRoutes não seja encontrado nenhuma rota para o element passado na Route, vai haver um redirecionamento para essa rota do navigate to */}
+            <Route path="*" element={ <Navigate to="/pagina-inicial"/> } />
+        </Routes>
+    );
+}
